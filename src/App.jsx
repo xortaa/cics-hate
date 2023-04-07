ca18da56d188a63090abadc1b5b3288672d6084b
xortaa <kercwinpaul@gmail.com>
2023-04-07 19:17:32 +0800

    hate cics >:(

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import "./App.css"
+
+function App() {
+
+  function generateRandomNumber() { 
+    const randomNumber = Math.floor(Math.random() * 4) + 1;
+    return randomNumber
+  }
+
+  return (
+    <div className='App'>
+      <img src="/sticker.png" alt="I HATE CICS >:(" className="image" />
+      <div className='marquee'>
+        <ul className={`marquee__content${generateRandomNumber()}`}>
+          <li>{`I HATE CICS >:(`}</li>
+          <li>{`I HATE CICS >:(`}</li>
+          <li>{`I HATE CICS >:(`}</li>
+          <li>{`I HATE CICS >:(`}</li>
+          <li>{`I HATE CICS >:(`}</li>
+        </ul>
+      </div>
+      <div className='marquee'>
+        <ul className={`marquee__content${generateRandomNumber()}`}>
+          <li>{`I HATE CICS >:(`}</li>
+          <li>{`I HATE CICS >:(`}</li>
+          <li>{`I HATE CICS >:(`}</li>
+          <li>{`I HATE CICS >:(`}</li>
+          <li>{`I HATE CICS >:(`}</li>
+        </ul>
+      </div>
+      <div className='marquee'>
+        <ul className={`marquee__content${generateRandomNumber()}`}>
+          <li>{`I HATE CICS >:(`}</li>
+          <li>{`I HATE CICS >:(`}</li>
+          <li>{`I HATE CICS >:(`}</li>
+          <li>{`I HATE CICS >:(`}</li>
+          <li>{`I HATE CICS >:(`}</li>
+        </ul>
+      </div>
+      <div className='marquee'>
+        <ul className={`marquee__content${generateRandomNumber()}`}>
+          <li>{`I HATE CICS >:(`}</li>
+          <li>{`I HATE CICS >:(`}</li>
+          <li>{`I HATE CICS >:(`}</li>
+          <li>{`I HATE CICS >:(`}</li>
+          <li>{`I HATE CICS >:(`}</li>
+        </ul>
+      </div>
+      <div className='marquee'>
+      <ul className={`marquee__content${generateRandomNumber()}`}>
+          <li>{`I HATE CICS >:(`}</li>
+          <li>{`I HATE CICS >:(`}</li>
+          <li>{`I HATE CICS >:(`}</li>
+          <li>{`I HATE CICS >:(`}</li>
+          <li>{`I HATE CICS >:(`}</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App
