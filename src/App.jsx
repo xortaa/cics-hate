2d5d9a5bde07d7512da1ffdcb1b8668951fe4434
xortaa <kercwinpaul@gmail.com>
2023-04-07 19:36:30 +0800

    hate cics :/

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,10 @@ function App() {
 
   return (
     <div className='App'>
-      <img src="/sticker.png" alt="I HATE CICS >:(" className="image" />
+      <img src="/sticker.png" alt="I HATE CICS >:(" className="image" onClick={() => {
+        console.log("I HATE CICS >:(( \n")
+        console.log("\n")
+      }}/>
       <div className='marquee'>
         <ul className={`marquee__content${generateRandomNumber()}`}>
           <li>{`I HATE CICS >:(`}</li>
